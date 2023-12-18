39030cb8e41d5a5260000d0331bd694737a43bc0
Sultan-Orunov <osbfswd@gmail.com>
2023-12-18 23:36:22 +0500

    загрузил шаблон админ панели Админа

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function adminDashboard(){
-        return view('admin.dashboard');
+        return view('admin.index');
     }
 }
