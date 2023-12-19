13b9acd6ec0ca5956f7db8eec4db4bdbf6a57796
Sultan-Orunov <osbfswd@gmail.com>
2023-12-19 23:17:55 +0500

    почистил шаблон администратора

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="page-footer">
-    <p class="mb-0">Copyright © 2022. All right reserved.</p>
+    <p class="mb-0">Copyright © 2023 - {{ date('Y') }} All right reserved.</p>
 </footer>
